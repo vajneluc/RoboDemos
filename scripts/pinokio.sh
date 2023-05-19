84d915758ae84c991e10386e6e0835531fe34480
Julius Tomsa <tomsa.julius@gmail.com>
2023-05-19 16:41:12 +0200

    added pinokio.sh

---
+++ scripts/pinokio.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export PATH=/opt/openrobots/bin:$PATH
+export PKG_CONFIG_PATH=/opt/openrobots/lib/pkgconfig:$PKG_CONFIG_PATH
+export LD_LIBRARY_PATH=/opt/openrobots/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=/opt/openrobots/lib/python3.10/site-packages:$PYTHONPATH # Adapt your desired python version here
+export CMAKE_PREFIX_PATH=/opt/openrobots:$CMAKE_PREFIX_PATH
