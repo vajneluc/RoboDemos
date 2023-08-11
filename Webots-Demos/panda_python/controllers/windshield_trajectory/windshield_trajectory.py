1e8d22addbcf87c03a8f8652b0ff0cfd97e5b3c5
JuliusTomsa <tomsa.julius@gmail.com>
2023-08-11 11:24:48 +0200

    working sphere trajectory waypoints

--- Webots-Demos/panda_python/controllers/windshield_trajectory/windshield_trajectory.py
+++ Webots-Demos/panda_python/controllers/windshield_trajectory/windshield_trajectory.py
@@ -6,9 +6,9 @@ from controller import Robot, Supervisor
 import math
 import pandas as pd
 import os
+import sys
 
-
-csv_path = '../../joints_moveit.csv'
+csv_path = '../../joints_moveit_smooth.csv'
 df = pd.read_csv(csv_path)
 
 TIME_STEP = 32
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     
     # create Supervisor instance
     supervisor = Supervisor()
-    print('this:', dir(supervisor.supervisor))
+    
     # panda motors
     motors = [robot.getDevice(f"panda_joint{i+1}") for i in range(7)]
     
@@ -41,36 +41,68 @@ if __name__ == "__main__":
         motors[i].setPosition(q)
     
     # create the robot node for reading EE position
-    robot_node = supervisor.getFromDef('GRIPPER')
-    print('not_this', dir(robot_node))
-    exit()
-    # create list for shape nodes
-    shape_nodes = []
-    '''
+    ee_node = supervisor.getFromDef('GRIPPER')
+    
+
+    # trace node
+    trace_node = supervisor.getFromDef('TRACEGROUP')
+
+    # children of root node
+    trace_children_field = trace_node.getField('children')
+    
+
+
     # Run through CSV Waypoints
     for i, row in df.iterrows():
-        if i % 10 == 0:
-            motors[Joints.JOINT1].setPosition(row['q0'])
-            motors[Joints.JOINT2].setPosition(row['q1'])
-            motors[Joints.JOINT3].setPosition(row['q2'])
-            motors[Joints.JOINT4].setPosition(row['q3'])
-            motors[Joints.JOINT5].setPosition(row['q4']-p2) # dunno why this works, but it corrects panda robot rotation
-            motors[Joints.JOINT6].setPosition(row['q5'])
-            motors[Joints.JOINT7].setPosition(row['q6'])
-            
-            # comparing position of simulation to the one read from csv (works with joints_moveit.csv)
-            translation_from_sim = robot_node.getPosition()
-            translation_from_csv = [row['ee_pos_x'], row['ee_pos_y'], row['ee_pos_z']]
-            offset = [translation_from_sim[i] - translation_from_csv[i] for i in range(3)]
-            print(f'sim_position {translation_from_sim}\ncsv_position {translation_from_csv}\noffset {offset}')
+        '''
+        # panda joints
+        motors[Joints.JOINT1].setPosition(row['panda_joint1'])
+        motors[Joints.JOINT2].setPosition(row['panda_joint2'])
+        motors[Joints.JOINT3].setPosition(row['panda_joint3'])
+        motors[Joints.JOINT4].setPosition(row['panda_joint4'])
+        motors[Joints.JOINT5].setPosition(row['panda_joint5']-p2) # dunno why this works, but it corrects panda robot rotation
+        motors[Joints.JOINT6].setPosition(row['panda_joint6'])
+        motors[Joints.JOINT7].setPosition(row['panda_joint7'])
+        '''
+        #q
+        motors[Joints.JOINT1].setPosition(row['q0'])
+        motors[Joints.JOINT2].setPosition(row['q1'])
+        motors[Joints.JOINT3].setPosition(row['q2'])
+        motors[Joints.JOINT4].setPosition(row['q3'])
+        motors[Joints.JOINT5].setPosition(row['q4']-p2) # dunno why this works, but it corrects panda robot rotation
+        motors[Joints.JOINT6].setPosition(row['q5'])
+        motors[Joints.JOINT7].setPosition(row['q6'])
 
-            
-            translation_string = f'{}'
-            children_field.importMFNodeFromString(-1, f'DEF BALL{i} Ball { "translation" {translation_from_sim[0]} }')
-            sphere = shape_node.addProto('Sphere')
-            sphere.getField('size').setSFVec3f([0.05, 0.05, 0.05])
-            shape_node.getField('translation').setSFVec3f(translation_from_sim)
-            
+
+        # Reading GRIPPER position and orientation
+        ee_sim_position = ee_node.getPosition()
+        ee_sim_orientation = ee_node.getOrientation()
+
+        # Printing GRIPPER position and orientation to console
+        print(f'sim_position {ee_sim_position}')
+        #print(f'sim_orientation {ee_sim_orientation}')  
+
+        z_vector = ee_sim_orientation[6:9]
+        length_of_panda_hand = 0.107
+        true_ee_position = [ee_sim_position[i] + z_vector[i] * length_of_panda_hand for i in range(3)]
+
+        if i % 10 == 0:
+            # Displaying spheres at each ee waypoint
+            trace_children_field.importMFNodeFromString(-1,
+                                                   'Pose { children [ Shape {'
+                                                   'geometry Sphere { radius 0.005 subdivision 3 } } ] }')
+
+            sphere_node = trace_children_field.getMFNode(-1)
+            translation_field = sphere_node.getField('translation')
+            translation_field.setSFVec3f(true_ee_position)
+
             # simulation wait time
             robot.step(TIME_STEP * 5)
-    '''
+    
+    while trace_children_field.getCount() > 0:
+        trace_children_field.removeMF(-1)
+        
+    # After goint throught all waypoints end the controller
+    sys.exit(0)
+        
+    
